8ef2e33ed25e15131d10165783cfb8c56a7af958
Nikita <ferr_um@mail.ru>
2015-10-25 12:11:31 +0300

    Изменение конфигов

--- config/web.php
+++ config/web.php
@@ -5,8 +5,41 @@ $params = require(__DIR__ . '/params.php');
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
+    'aliases' => ['@module1' => 'modules'],
     'bootstrap' => ['log'],
+    'modules' => ['test' => ['class' => 'app\modules\test\Module'],
+                  'admin' => ['class' => 'app\modules\admin\Module',],   
+                  ],
+
     'components' => [
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'pattern' => '<controller>/<action>/<id:\d+>',
+                    'route' => '<controller>/<action>',
+                    'suffix' => ''
+                ],
+                [
+                'pattern' => '<controller>/<action>',
+                    'route' => '<controller>/<action>',
+                    'suffix' => '.php'
+                ],
+                [
+                    'pattern' => '<module>/<controller>/<action>/<id:\d+>',
+                    'route' => '<module>/<controller>/<action>',
+                    'suffix' => ''
+                ],
+                [
+                    'pattern' => '<module>/<controller>/<action>',
+                    'route' => '<module>/<controller>/<action>',
+                    'suffix' => '.php'
+                ]
+            ],
+        ],
+
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '_9LCsN0TI0OU9wnmY4KFvU3WsleDY2UH',
@@ -45,6 +78,11 @@ $config = [
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
+    $config['bootstrap'][] = 'gii';
+
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
